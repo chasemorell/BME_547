d82b592ef2ce8fcbe45eea26fcf7e06e0a7768fe
Chase Morell <chase.morell@duke.edu>
2022-09-21 13:09:31 -0400

    updated reqs 5

--- dosing.py
+++ dosing.py
@@ -25,13 +25,12 @@ def dose_amount():
     if units == "lb":
         weight = weight / 2.205
     dosages_mg_per_kg = [30, 10, 10, 12]
-    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis - 1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
-    print("  the correct dosage is {:.1f} mg the first day"
-          .format(dosage_mg_first_day))
+    print("  the correct dosage is {:.1f} mg the first day".format(dosage_mg_first_day))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dose_amount()
--- weight_entry.py
+++ weight_entry.py
@@ -2,12 +2,14 @@ def input_weight_entry():
     print("Enter patient weight in form of ## units (e.g., 105 lb)")
     weight_input = input("Enter weight: ")
     weight_in_kg = parse_weight_input(weight_input)
-    print("The patient weight of {} kg will be stored "
-          "in database.".format(weight_in_kg))
+    print(
+        "The patient weight of {} kg will be stored "
+        "in database.".format(weight_in_kg)
+    )
 
 
 def parse_weight_input(weight_input):
-    weight, units = weight_input.split(' ')
+    weight, units = weight_input.split(" ")
     weight = int(weight)
     if units == "lb":
         weight_kg = convert_lb_to_kg(weight)
